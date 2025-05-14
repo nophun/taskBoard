4dd5c0c09425c5993c023bc0c0fff9f58b839522
Liangyu Chen <liangyu.chen@vaisala.com>
2025-05-14 20:10:46 +0300

    Delete GUI program from this repo. Use submodule instead.

--- make_exe.py
+++ make_exe.py
@@ -1,36 +1,17 @@
-# -*- coding: utf-8 -*-
-"""Make single-file executable for CSOLVER2.
-
-The executable is created by running this Python script. Before running it, one
-verify and edit the general information, especially the program file location,
-below. The program file CSOLVER2.pyw must be in a local folder (not a network
-drive folder) together with the other required folder CSOLVER2_files. The
-executable is created from these using PyInstaller and at least version 3.3.1
-is required.
-
-This script was modified from a similar script for making executables for the
-DD-23 Updater and DD-23 Timeout Configurator (by Liangyu Chen, Janesko Oy,
-20.2.2019).
-
-Created on Aug 24 2021
-Last edit on Aug 25 2021
-@author: ANJOK2
-"""
-
 import os
 from PyInstaller.utils.win32.versioninfo import (
     StringStruct, VSVersionInfo, FixedFileInfo, StringFileInfo, StringTable,
     VarFileInfo, VarStruct)
 import PyInstaller.__main__
+import shutil
 
-# General parameters (EDIT THESE ONLY):
-SCRIPT_NAME = 'TaskBoard.py'
-SCRIPT_LOCATION = (os.getcwd())  # folder of the program files
+SCRIPT_NAME = 'gui/src/TaskBoard.py'
+SCRIPT_LOCATION = (os.getcwd())
 APP_NAME = 'TASKBOARD'
 EXE_NAME = 'TASKBOARD'
 VERSION = '1.3'
 VERSIONa = (1, 3, 0, 0)
-ICON = 'Yuster.ico'
+ICON = 'gui/logo/Yuster.ico'
 COMPANY = 'Yuster electronics'
 COPYRIGHT = 'Yuster electronics'
 
@@ -65,4 +46,4 @@ PyInstaller.__main__.run([f'{SCRIPT_NAME}',
                           '--version-file=version.temp',
                           '--noconsole'])
 os.unlink('version.temp')
-
+shutil.copyfile(ICON, 'dist/Yuster.ico')
